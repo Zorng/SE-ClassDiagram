18dfd5cafce8a753fe40c25949e4cf89b19abe0d
araeht <meakputsoktheara13@gmail.com>
2025-06-12 19:58:18 +0700

    sample

--- src/School/Department.java
+++ src/School/Department.java
@@ -1,4 +1,23 @@
 package School;
 
 public class Department {
+    private String departmentID;
+    private String departmentName;
+    private String departmentHead;
+
+    
+    public Department(String departmentID, String departmentName, String departmentHead) {
+        this.departmentID = departmentID;
+        this.departmentName = departmentName;
+        this.departmentHead = departmentHead;
+    }
+
+   
+    public Department(String departmentID, String departmentName) {
+        this.departmentID = departmentID;
+        this.departmentName = departmentName;
+    }
+
+    
+    public Department() {}
 }
